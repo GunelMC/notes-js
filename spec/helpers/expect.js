1cb9b5d4f3d7a89bdaaa0bf13e2d3f2de2ba68c6
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2021-06-10 16:13:17 +0100

    added toThrowError()

--- spec/helpers/expect.js
+++ spec/helpers/expect.js
@@ -15,6 +15,21 @@ function expect(a) {
         console.log("Fail")
       }   
     },
+
+    toThrowError: function(b) {
+      try {
+        a()
+        console.log("Fail")
+      }
+      catch (exception_var) {
+        if (exception_var === b) {
+          console.log("Pass")
+        } else {
+        console.log(`Fail. Expected ${b}; Got ${exception_var}`)
+        }
+      }
+    },
+
     toHaveBeenCalledWithArguments: function(b) {
       if (a.getCallArguments === b) {
         console.log("Pass")
