b316d98636a6acefdafc6a3bb05d97c4ba194ddd
Robert Dupplaw <dupplawrobert@gmail.com>
2021-06-10 21:06:13 +0100

    refactored NoteView tests (which are failing)

--- spec/noteViewSpec.js
+++ spec/noteViewSpec.js
@@ -1,17 +1,13 @@
+describe('NoteView', () => {
+  const noteListElementDouble = document.createElement('div');
+  const noteElementDouble = document.createElement('div');
+  const noteAdderElementDouble = document.createElement('div');
 
-const noteDivMock = document.createElement('div');
-const noteView = new NoteView(noteDivMock)
-
-function testViewRender() {
-  const noteView = new NoteView(["yes","no","maybe"])
-  assert.isTrue(noteView.renderNoteList() === "yes")
-}
-
-it("should display all notes in inner html", function() {
-  noteView.renderNoteList([{text:"hi"}])
-  expect(noteDivMock.innerHTML).toEqual(`<ul><li><a href="#0">hi</a></li></ul>`)
-})
-
-it("should create an instance of noteView", function() {
-  expect(noteView).toBeInstanceOf(NoteView)
+  describe('.prototype.renderNoteList()', () => {
+    it('puts <ul> of notes in the noteListElement', () => {
+      const noteView = new NoteView(noteListElementDouble, noteElementDouble, noteAdderElementDouble)
+      noteView.renderNoteList([{text: 'test1'}, {text: 'test2'}, {text: 'test3'}])
+      expect(noteListElementDouble.innerHTML).toBe('<ul><li><a href="#0">test1</a></li><li><a href="#1">test2</a></li><li><a href="#2">test3</a></li></ul>')
+    })
+  })
 })
